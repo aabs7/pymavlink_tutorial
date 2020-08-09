7a5e2940047df32c9af8beb2f66b7256cd9b762f
Abhish Khanal <abheeshkhanal@gmail.com>
2020-08-09 12:11:22 +0545

    specific message add

---
+++ specific_msg.py
@@ -0,0 +1,29 @@
+"""
+Example of how to connect pymavlink to an autopilot via an UDP connection
+"""
+import time
+# Import mavutil
+from pymavlink import mavutil
+
+import sys
+
+# Create the connection
+#  If using a companion computer
+#  the default connection is available
+#  at ip 192.168.2.1 and the port 14550
+# Note: The connection is done with 'udpin' and not 'udpout'.
+#  You can check in http:192.168.2.2:2770/mavproxy that the communication made for 14550
+#  uses a 'udpbcast' (client) and not 'udpin' (server).
+#  If you want to use QGroundControl in parallel with your python script,
+#  it's possible to add a new output port in http:192.168.2.2:2770/mavproxy as a new line.
+#  E.g: --out udpbcast:192.168.2.255:yourport
+master = mavutil.mavlink_connection('tcp:127.0.0.1:5762')
+
+# Get some information !
+while True:
+    try:
+        print(master.recv_match(type = 'LOCAL_POSITION_NED',blocking=True))
+    except:
+        pass
+    time.sleep(0.1)
+    
